db3d375c90d7d82a8339c1a2ee97b1384a951166
Dan Williams <dan@dotfive.co.uk>
2023-05-31 08:21:36 +0100

    Added ip!() macro for IpAddr creation sugar

--- crates/rubedo/src/tests/sugar.rs
+++ crates/rubedo/src/tests/sugar.rs
@@ -40,4 +40,42 @@ mod s {
 	}
 }
 
+//		ip!																		
+#[cfg(test)]
+mod ip {
+	use super::super::*;
+	use std::net::Ipv4Addr;
+	
+	//		ip!																	
+	#[test]
+	fn ip__empty() {
+		assert_eq!(ip!(), Ipv4Addr::new(0, 0, 0, 0));
+	}
+	#[test]
+	fn ip__str() {
+		assert_eq!(ip!("1.2.3.4"), Ipv4Addr::new(1, 2, 3, 4));
+	}
+	#[test]
+	#[should_panic]
+	fn ip__str_invalid_1() {
+		ip!("1.2.3");
+	}
+	#[test]
+	#[should_panic]
+	fn ip__str_invalid_2() {
+		ip!("1.2.3.4.5");
+	}
+	#[test]
+	#[should_panic]
+	fn ip__str_invalid_3() {
+		ip!("1.2.3.999");
+	}
+	#[test]
+	fn ip__args() {
+		//	There are no tests for this style failing, as the compiler will
+		//	enforce the correct number of arguments and that each is a valid u8.
+		assert_eq!(ip!(1, 2, 3, 4), Ipv4Addr::new(1, 2, 3, 4));
+	}
+}
+
 
