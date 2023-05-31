04196e03bfa5a7d123801b4b08213dc45257d0ae
Dan Williams <dan@dotfive.co.uk>
2023-05-31 15:40:13 +0100

    Converted ip!() to a proc_macro
      - Converted the ip!() macro from a rule-based macro to a procedural
        macro. This allows it more control over syntax, and so it now
        accepts IP addresses in raw 1.2.3.4 form, e.g. ip!(1.2.3.4).
    
      - Updated all tests. Note that the should_panic tests will currently
        cause compilation to fail, as they are compile-time panics. They
        need to be converted to a different type of test.
    
      - The tests now live in the top-level tests folder, as they are best
        run as integration tests given that the library has to be compiled
        first in order to test them.

--- crates/rubedo-macros/src/lib.rs
+++ crates/rubedo-macros/src/lib.rs
@@ -1,6 +1,7 @@
 //		Packages
 
-#[allow(unused_imports)]
+use proc_macro::{TokenStream, TokenTree};
+use quote::quote;
 use std::net::IpAddr;
 
 
@@ -33,6 +34,7 @@ use std::net::IpAddr;
 /// use rubedo::ip;
 /// use std::net::IpAddr;
 /// 
+/// assert_eq!(ip!(1.2.3.4), IpAddr::from([1, 2, 3, 4]));
 /// assert_eq!(ip!("1.2.3.4"), IpAddr::from([1, 2, 3, 4]));
 /// assert_eq!(ip!(1, 2, 3, 4), IpAddr::from([1, 2, 3, 4]));
 /// ```
@@ -41,17 +43,90 @@ use std::net::IpAddr;
 /// 
 /// * [`IpAddr`]
 /// 
-#[macro_export]
-macro_rules! ip {
-	($s:expr) => {
-		$s.parse::<IpAddr>().unwrap()
-	};
-	($a:expr, $b:expr, $c:expr, $d:expr) => {
-		IpAddr::from([$a, $b, $c, $d])
-	};
-	() => {
-		IpAddr::from([0, 0, 0, 0])
-	};
+#[proc_macro]
+pub fn ip(input: TokenStream) -> TokenStream {
+	//	We should not use things from proc_macro::TokenStream too deeply. Here's
+	//	the reason why:
+	//	
+	//	https://fasterthanli.me/articles/proc-macro-support-in-rust-analyzer-for-nightly-rustc-versions
+	//	
+	//	Not only does attempting to use proc_macro::bridge give an error saying
+	//	"use of unstable library feature 'proc_macro_internals'", but also even
+	//	if flags were added to access the internals, it seems they are subject
+	//	to change without warning. This means that we cannot access things like
+	//	LitKind (the kind property of a TokenStream token of type Literal, e.g.
+	//	Literal.kind = LitKind::Float) and therefore have to rely upon the other
+	//	properties and do some rudimentary and naive type detection.
+	//	
+	//	https://github.com/rust-lang/rust/blob/master/library/proc_macro/src/bridge/mod.rs
+	//	
+	//	Unfortunately, this means that as we cannot look at the Literal.kind
+	//	property to get an authoritative statement about the type, the guesswork
+	//	process we are forced to use means we cannot tell the difference between
+	//	code that is written as 1.2.3.4 versus "1.2"."3.4". Both cases will lead
+	//	to a literal, followed by punctuation, followed by another literal. The
+	//	first one will be two Literals of kind Float, and the second will be two
+	//	Literals of type Str. We would ideally like to consider as technically
+	//	invalid, but are bound to accept it due to this lack of differentiation
+	//	ability. Hopefully this will change in future.
+	//	
+	//	Other than that, it is not advantageous to use proc_macro2, as it does
+	//	not provide anything we cannot already access or do, and using syn is a
+	//	problem for two reasons: firstly and most importantly that it enforces
+	//	Rust grammar and is not merely a method of accessing tokens, meaning
+	//	that it will complain about our custom syntax; and secondly, it is quite
+	//	a heavy package and if we don't need it then it's nice to avoid the
+	//	compilation overhead and increased binary size. (Note: It would probably
+	//	be possible to use peek() in some way to adjust the grammar checking,
+	//	and indeed it looks like the grammar checking could potentially be
+	//	controlled or circumvented by defining a custom struct to use in the
+	//	parse_macro_input!(input as T) call, but this seems like too much to do
+	//	for something so simple, and on balance an undesirable approach.)
+	let mut str = String::new();
+	for (count, token) in input.into_iter().enumerate() {
+		//	If there's only one token, it should be a string literal to be valid. We
+		//	cannot confirm what kind of Literal it is, but we can check that it's a
+		//	Literal and then try to parse it into an IpAddr.
+		//	
+		//	If there are three tokens, this should correspond to 1.2.3.4 syntax.
+		//	
+		//	If there are seven tokens, this should correspond to 1, 2, 3, 4 syntax.
+		if count == 0 || count == 2 || count == 4 || count == 6 {
+			match token {
+				TokenTree::Literal(lit) => str.push_str(&lit.to_string().replace('"', "")),
+				_                       => panic!("Invalid IP address")
+			}
+		} else if count == 1 || count == 3 || count == 5 {
+			match token {
+				TokenTree::Punct(punct) => {
+					match punct.to_string().chars().next().unwrap() {
+						'.'             => str.push('.'),
+						','             => str.push(','),
+						_               => panic!("Invalid IP address")
+					}
+				},
+				_                       => panic!("Invalid IP address")
+			}
+		} else {
+			panic!("Invalid IP address")
+		}
+	}
+	if str.is_empty() {
+		return quote! {
+			std::net::IpAddr::from([0, 0, 0, 0])
+		}.into();
+	}
+	//	We should not have more than 3 dots, more than 3 commas, or a mixture of
+	//	dots and commas.
+	let count_dots   = str.matches('.').count();
+	let count_commas = str.matches(',').count();
+	if !((count_dots == 3 && count_commas == 0) || (count_dots == 0 && count_commas == 3)) {
+		panic!("Invalid IP address");
+	}
+	let ip_addr      = str.replace(',', ".").parse::<IpAddr>().expect("Invalid IP address").to_string();
+	quote! {
+		#ip_addr.parse::<std::net::IpAddr>().unwrap()
+	}.into()
 }
 
 
--- crates/rubedo-macros/tests/lib.rs
+++ crates/rubedo-macros/tests/lib.rs
@@ -1,4 +1,5 @@
 #![allow(non_snake_case)]
+#![allow(unused_imports)]
 
 //		Tests
 
@@ -6,15 +7,19 @@
 #[cfg(test)]
 mod ip {
 	use rubedo_macros::*;
-	use std::net::{IpAddr, Ipv4Addr};
+	use std::net::Ipv4Addr;
 	
 	//		ip!																	
 	#[test]
-	fn ip__empty() {
+	fn ip__empty_1() {
 		assert_eq!(ip!(), Ipv4Addr::new(0, 0, 0, 0));
 	}
 	#[test]
-	fn ip__str() {
+	fn ip__empty_2() {
+		assert_eq!(ip!(""), Ipv4Addr::new(0, 0, 0, 0));
+	}
+	#[test]
+	fn ip__str_valid() {
 		assert_eq!(ip!("1.2.3.4"), Ipv4Addr::new(1, 2, 3, 4));
 	}
 	#[test]
@@ -33,10 +38,61 @@ mod ip {
 		ip!("1.2.3.999");
 	}
 	#[test]
-	fn ip__args() {
-		//	There are no tests for this style failing, as the compiler will
-		//	enforce the correct number of arguments and that each is a valid u8.
+	fn ip__args_valid() {
 		assert_eq!(ip!(1, 2, 3, 4), Ipv4Addr::new(1, 2, 3, 4));
+	}
+	#[test]
+	#[should_panic]
+	fn ip__args_invalid_1() {
+		ip!(1, 2, 3);
+	}
+	#[test]
+	#[should_panic]
+	fn ip__args_invalid_2() {
+		ip!(1, 2, 3, 4, 5);
+	}
+	#[test]
+	#[should_panic]
+	fn ip__args_invalid_3() {
+		ip!(1, 2, 3, 999);
+	}
+	#[test]
+	fn ip__raw_valid_1() {
+		assert_eq!(ip!(1.2.3.4), Ipv4Addr::new(1, 2, 3, 4));
+	}
+	#[test]
+	fn ip__raw_valid_2() {
+		assert_eq!(ip!(1 . 2 . 3 . 4), Ipv4Addr::new(1, 2, 3, 4));
+	}
+	#[test]
+	#[should_panic]
+	fn ip__raw_invalid_1() {
+		ip!(1.2.3);
+	}
+	#[test]
+	#[should_panic]
+	fn ip__raw_invalid_2() {
+		ip!(1.2.3.4.5);
+	}
+	#[test]
+	#[should_panic]
+	fn ip__raw_invalid_3() {
+		ip!(1.2.3.999);
+	}
+	#[test]
+	fn ip__mixed_invalid() {
+		//	We don't really want this, but it's a side-effect of the parser.
+		assert_eq!(ip!("1.2".3.4), Ipv4Addr::new(1, 2, 3, 4));
+	}
+	#[test]
+	#[should_panic]
+	fn ip__mixed_invalid_1() {
+		ip!(1.2,3.4);
+	}
+	#[test]
+	#[should_panic]
+	fn ip__mixed_invalid_2() {
+		ip!(1,2.3.4);
 	}
 }
 
