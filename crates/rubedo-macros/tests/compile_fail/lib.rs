fb7770897e253a3602ceff44d46048a649cb185f
Dan Williams <dan@dotfive.co.uk>
2023-05-31 16:06:07 +0100

    Added trybuild to catch proc_macro test failures
      - trybuild allow us to catch the various panics emitted by the testing
        of proc_macro failure cases. This has been added, and the failure
        tests have been moved to a separate folder along with an output
        expectations file.
    
      - Tests that are expected to pass remain in the tests folder, and the
        running of trybuild now happens from there as one single aggregated
        test. The saved output allows granularity of results for inspection.

---
+++ crates/rubedo-macros/tests/compile_fail/lib.rs
@@ -0,0 +1,48 @@
+#![allow(non_snake_case)]
+#![allow(unused_imports)]
+
+//		Tests
+
+fn main() {
+	use rubedo_macros::*;
+	use std::net::IpAddr;
+	
+	//		ip!																	
+	//	These tests are here as it is is not possible to test a proc_macro in
+	//	the same crate as it is defined.
+	fn ip__str_invalid_1() {
+		ip!("1.2.3");
+	}
+	fn ip__str_invalid_2() {
+		ip!("1.2.3.4.5");
+	}
+	fn ip__str_invalid_3() {
+		ip!("1.2.3.999");
+	}
+	fn ip__args_invalid_1() {
+		ip!(1, 2, 3);
+	}
+	fn ip__args_invalid_2() {
+		ip!(1, 2, 3, 4, 5);
+	}
+	fn ip__args_invalid_3() {
+		ip!(1, 2, 3, 999);
+	}
+	fn ip__raw_invalid_1() {
+		ip!(1.2.3);
+	}
+	fn ip__raw_invalid_2() {
+		ip!(1.2.3.4.5);
+	}
+	fn ip__raw_invalid_3() {
+		ip!(1.2.3.999);
+	}
+	fn ip__mixed_invalid_1() {
+		ip!(1.2,3.4);
+	}
+	fn ip__mixed_invalid_2() {
+		ip!(1,2.3.4);
+	}
+}
+
+
--- crates/rubedo-macros/tests/lib.rs
+++ crates/rubedo-macros/tests/lib.rs
@@ -23,38 +23,8 @@ mod ip {
 		assert_eq!(ip!("1.2.3.4"), Ipv4Addr::new(1, 2, 3, 4));
 	}
 	#[test]
-	#[should_panic]
-	fn ip__str_invalid_1() {
-		ip!("1.2.3");
-	}
-	#[test]
-	#[should_panic]
-	fn ip__str_invalid_2() {
-		ip!("1.2.3.4.5");
-	}
-	#[test]
-	#[should_panic]
-	fn ip__str_invalid_3() {
-		ip!("1.2.3.999");
-	}
-	#[test]
 	fn ip__args_valid() {
 		assert_eq!(ip!(1, 2, 3, 4), Ipv4Addr::new(1, 2, 3, 4));
-	}
-	#[test]
-	#[should_panic]
-	fn ip__args_invalid_1() {
-		ip!(1, 2, 3);
-	}
-	#[test]
-	#[should_panic]
-	fn ip__args_invalid_2() {
-		ip!(1, 2, 3, 4, 5);
-	}
-	#[test]
-	#[should_panic]
-	fn ip__args_invalid_3() {
-		ip!(1, 2, 3, 999);
 	}
 	#[test]
 	fn ip__raw_valid_1() {
@@ -65,34 +35,13 @@ mod ip {
 		assert_eq!(ip!(1 . 2 . 3 . 4), Ipv4Addr::new(1, 2, 3, 4));
 	}
 	#[test]
-	#[should_panic]
-	fn ip__raw_invalid_1() {
-		ip!(1.2.3);
-	}
-	#[test]
-	#[should_panic]
-	fn ip__raw_invalid_2() {
-		ip!(1.2.3.4.5);
-	}
-	#[test]
-	#[should_panic]
-	fn ip__raw_invalid_3() {
-		ip!(1.2.3.999);
-	}
-	#[test]
 	fn ip__mixed_invalid() {
 		//	We don't really want this, but it's a side-effect of the parser.
 		assert_eq!(ip!("1.2".3.4), Ipv4Addr::new(1, 2, 3, 4));
 	}
 	#[test]
-	#[should_panic]
-	fn ip__mixed_invalid_1() {
-		ip!(1.2,3.4);
-	}
-	#[test]
-	#[should_panic]
-	fn ip__mixed_invalid_2() {
-		ip!(1,2.3.4);
+	fn compile_fail() {
+		trybuild::TestCases::new().compile_fail("tests/compile_fail/lib.rs");
 	}
 }
 
