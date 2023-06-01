14555c3cffac258e2496d227859d7bbd4584e0a6
Dan Williams <dan@dotfive.co.uk>
2023-06-01 13:23:06 +0100

    Added chrono::NaiveDateExt
      - Added a range of functions to extend chrono::NaiveDate.

--- crates/rubedo/src/chrono.rs
+++ crates/rubedo/src/chrono.rs
@@ -8,8 +8,223 @@ mod tests;
 
 //		Packages
 
+use chrono::{prelude::*, NaiveDate, Utc};
+
 
 
 //		Traits
+
+//§		NaiveDateExt															
+/// This trait provides additional functionality to [`NaiveDate`].
+pub trait NaiveDateExt {
+	//		today																
+	/// Returns the current date.
+	/// 
+	/// Although this is a static method, it does not return an [`Option`] as it
+	/// cannot fail.
+	fn today() -> NaiveDate;
+	
+	//		days_in_month														
+	/// Returns the number of days in the date's month.
+	/// 
+	/// This method operates on `&self`. For the equivalent method that operates
+	/// without an instance, see [`days_in_month_opt()`](NaiveDateExt::days_in_month_opt()).
+	fn days_in_month(&self) -> u32;
+	
+	//		days_in_month_opt														
+	/// Returns the number of days in the given month.
+	/// 
+	/// This method operates without an instance. For the equivalent method that
+	/// operates on `&self`, see [`days_in_month()`](NaiveDateExt::days_in_month()).
+	///
+	/// The outcome is wrapped in an [`Option`]. If the given year or month is
+	/// invalid, `None` is returned.
+	fn days_in_month_opt(year: i32, month: u32) -> Option<u32>;
+	
+	//		days_in_year														
+	/// Returns the number of days in the date's year.
+	/// 
+	/// This method operates on `&self`. For the equivalent method that operates
+	/// without an instance, see [`days_in_year_opt()`](NaiveDateExt::days_in_year_opt()).
+	fn days_in_year(&self) -> u32;
+	
+	//		days_in_year_opt														
+	/// Returns the number of days in the given year.
+	/// 
+	/// This method operates without an instance. For the equivalent method that
+	/// operates on `&self`, see [`days_in_year()`](NaiveDateExt::days_in_year()).
+	///
+	/// The outcome is wrapped in an [`Option`]. If the given year is invalid,
+	/// `None` is returned.
+	fn days_in_year_opt(year: i32) -> Option<u32>;
+	
+	//		is_leap_year														
+	/// Returns `true` if the date's year is a leap year.
+	/// 
+	/// This method operates on `&self`. For the equivalent method that operates
+	/// without an instance, see [`is_leap_year_opt()`](NaiveDateExt::is_leap_year_opt()).
+	fn is_leap_year(&self) -> bool;
+	
+	//		is_leap_year_opt														
+	/// Returns `true` if the given year is a leap year.
+	/// 
+	/// This method operates without an instance. For the equivalent method that
+	/// operates on `&self`, see [`is_leap_year()`](NaiveDateExt::is_leap_year()).
+	/// 
+	/// The outcome is wrapped in an [`Option`]. If the given year is invalid,
+	/// `None` is returned.
+	fn is_leap_year_opt(year: i32) -> Option<bool>;
+	
+	//		start_of_month														
+	/// Returns the date of the first day of the date's month.
+	/// 
+	/// This method operates on `&self`. For the equivalent method that operates
+	/// without an instance, see [`start_of_month_opt()`](NaiveDateExt::start_of_month_opt()).
+	fn start_of_month(&self) -> NaiveDate;
+	
+	//		start_of_month_opt														
+	/// Returns the date of the first day of the given month.
+	/// 
+	/// This method operates without an instance. For the equivalent method that
+	/// operates on `&self`, see [`start_of_month()`](NaiveDateExt::start_of_month()).
+	///
+	/// The outcome is wrapped in an [`Option`]. If the given year or month is
+	/// invalid, `None` is returned.
+	fn start_of_month_opt(year: i32, month: u32) -> Option<NaiveDate>;
+	
+	//		end_of_month														
+	/// Returns the date of the last day of the date's month.
+	/// 
+	/// This method operates on `&self`. For the equivalent method that operates
+	/// without an instance, see [`end_of_month_opt()`](NaiveDateExt::end_of_month_opt()).
+	fn end_of_month(&self) -> NaiveDate;
+	
+	//		end_of_month_opt														
+	/// Returns the date of the last day of the given month.
+	/// 
+	/// This method operates without an instance. For the equivalent method that
+	/// operates on `&self`, see [`end_of_month()`](NaiveDateExt::end_of_month()).
+	///
+	/// The outcome is wrapped in an [`Option`]. If the given year or month is
+	/// invalid, `None` is returned.
+	fn end_of_month_opt(year: i32, month: u32) -> Option<NaiveDate>;
+	
+	//		start_of_year														
+	/// Returns the date of the first day of the date's year.
+	/// 
+	/// This method operates on `&self`. For the equivalent method that operates
+	/// without an instance, see [`start_of_year_opt()`](NaiveDateExt::start_of_year_opt()).
+	fn start_of_year(&self) -> NaiveDate;
+	
+	//		start_of_year_opt														
+	/// Returns the date of the first day of the given year.
+	/// 
+	/// This method operates without an instance. For the equivalent method that
+	/// operates on `&self`, see [`start_of_year()`](NaiveDateExt::start_of_year()).
+	///
+	/// The outcome is wrapped in an [`Option`]. If the given year is invalid,
+	/// `None` is returned.
+	fn start_of_year_opt(year: i32) -> Option<NaiveDate>;
+	
+	//		end_of_year															
+	/// Returns the date of the last day of the date's year.
+	/// 
+	/// This method operates on `&self`. For the equivalent method that operates
+	/// without an instance, see [`end_of_year_opt()`](NaiveDateExt::end_of_year_opt()).
+	fn end_of_year(&self) -> NaiveDate;
+	
+	//		end_of_year_opt															
+	/// Returns the date of the last day of the given year.
+	/// 
+	/// This method operates without an instance. For the equivalent method that
+	/// operates on `&self`, see [`end_of_year()`](NaiveDateExt::end_of_year()).
+	///
+	/// The outcome is wrapped in an [`Option`]. If the given year is invalid,
+	/// `None` is returned.
+	fn end_of_year_opt(year: i32) -> Option<NaiveDate>;
+}
+
+impl NaiveDateExt for NaiveDate {
+	//		today																
+	fn today() -> Self {
+		Utc::now().date_naive()
+	}
+	
+	//		days_in_month														
+	fn days_in_month(&self) -> u32 {
+		Self::days_in_month_opt(self.year(), self.month()).unwrap()
+	}
+	
+	//		days_in_month_opt													
+	fn days_in_month_opt(year: i32, month: u32) -> Option<u32> {
+		Self::end_of_month_opt(year, month).map(|date| date.day())
+	}
+	
+	//		days_in_year														
+	fn days_in_year(&self) -> u32 {
+		Self::days_in_year_opt(self.year()).unwrap()
+	}
+	
+	//		days_in_year_opt													
+	fn days_in_year_opt(year: i32) -> Option<u32> {
+		Self::from_ymd_opt(year, 2, 1).map(|_| if Self::from_ymd_opt(year, 2, 29).is_some() { 366 } else { 365 })
+	}
+	
+	//		is_leap_year														
+	fn is_leap_year(&self) -> bool {
+		Self::is_leap_year_opt(self.year()).unwrap()
+	}
+	
+	//		is_leap_year_opt													
+	fn is_leap_year_opt(year: i32) -> Option<bool> {
+		Self::from_ymd_opt(year, 2, 1).map(|_| Self::from_ymd_opt(year, 2, 29).is_some())
+	}
+	
+	//		start_of_month														
+	fn start_of_month(&self) -> Self {
+		Self::start_of_month_opt(self.year(), self.month()).unwrap()
+	}
+	
+	//		start_of_month_opt													
+	fn start_of_month_opt(year: i32, month: u32) -> Option<Self> {
+		Self::from_ymd_opt(year, month, 1)
+	}
+	
+	//		end_of_month														
+	fn end_of_month(&self) -> Self {
+		Self::end_of_month_opt(self.year(), self.month()).unwrap()
+	}
+	
+	//		end_of_month_opt													
+	fn end_of_month_opt(year: i32, month: u32) -> Option<Self> {
+		match Self::from_ymd_opt(year, month, 1) {
+			Some(_)         => match Self::from_ymd_opt(year, month + 1, 1).or(Self::from_ymd_opt(year + 1, 1, 1)) {
+				Some(date) => date.pred_opt(),
+				None       => None,
+			},
+			None           => None,
+		}
+	}
+	
+	//		start_of_year														
+	fn start_of_year(&self) -> Self {
+		Self::start_of_year_opt(self.year()).unwrap()
+	}
+	
+	//		start_of_year_opt													
+	fn start_of_year_opt(year: i32) -> Option<Self> {
+		Self::from_ymd_opt(year, 1, 1)
+	}
+	
+	//		end_of_year															
+	fn end_of_year(&self) -> Self {
+		Self::end_of_year_opt(self.year()).unwrap()
+	}
+	
+	//		end_of_year_opt														
+	fn end_of_year_opt(year: i32) -> Option<Self> {
+		Self::from_ymd_opt(year, 12, 31)
+	}
+}
 
 
--- crates/rubedo/src/tests/chrono.rs
+++ crates/rubedo/src/tests/chrono.rs
@@ -1,9 +1,140 @@
 #![allow(non_snake_case)]
 
 //		Tests
+
+//§		NaiveDateExt															
 #[cfg(test)]
-mod tests {
+mod naivedate_ext {
 	use super::super::*;
+	
+	//		today																
+	#[test]
+	fn today() {
+		//	There is a very small possibility that this test will fail if the
+		//	test is run at the exact moment that the date changes.
+		let timestamp = Utc::now().timestamp();
+		let datetime  = NaiveDateTime::from_timestamp_opt(timestamp, 0).unwrap();
+		let today     = NaiveDate::today();
+		assert_eq!(today.year(),  datetime.year());
+		assert_eq!(today.month(), datetime.month());
+		assert_eq!(today.day(),   datetime.day());
+	}
+	
+	//		days_in_month														
+	#[test]
+	fn days_in_month() {
+		let date = NaiveDate::from_ymd_opt(2000, 1, 1).unwrap();
+		assert_eq!(date.days_in_month(), 31);
+	}
+	
+	//		days_in_month_opt													
+	#[test]
+	fn days_in_month_opt__valid() {
+		assert_eq!(NaiveDate::days_in_month_opt(2000, 1).unwrap(), 31);
+	}
+	#[test]
+	fn days_in_month_opt__invalid() {
+		assert!(NaiveDate::days_in_month_opt(2000, 13).is_none());
+	}
+	
+	//		days_in_year														
+	#[test]
+	fn days_in_year__normal() {
+		let date = NaiveDate::from_ymd_opt(2001, 1, 1).unwrap();
+		assert_eq!(date.days_in_year(), 365);
+	}
+	#[test]
+	fn days_in_year__leap() {
+		let date = NaiveDate::from_ymd_opt(2000, 1, 1).unwrap();
+		assert_eq!(date.days_in_year(), 366);
+	}
+	
+	//		days_in_year_opt													
+	#[test]
+	fn days_in_year_opt__normal() {
+		assert_eq!(NaiveDate::days_in_year_opt(2001).unwrap(), 365);
+	}
+	#[test]
+	fn days_in_year_opt__leap() {
+		assert_eq!(NaiveDate::days_in_year_opt(2000).unwrap(), 366);
+	}
+	
+	//		is_leap_year														
+	#[test]
+	fn is_leap_year() {
+		let date = NaiveDate::from_ymd_opt(2000, 1, 1).unwrap();
+		assert_eq!(date.is_leap_year(), true);
+		
+		let date = NaiveDate::from_ymd_opt(2001, 1, 1).unwrap();
+		assert_eq!(date.is_leap_year(), false);
+	}
+	
+	//		is_leap_year_opt													
+	#[test]
+	fn is_leap_year_opt() {
+		assert_eq!(NaiveDate::is_leap_year_opt(2000).unwrap(), true);
+		assert_eq!(NaiveDate::is_leap_year_opt(2001).unwrap(), false);
+	}
+	
+	//		start_of_month														
+	#[test]
+	fn start_of_month() {
+		let date = NaiveDate::from_ymd_opt(2000, 1, 20).unwrap();
+		assert_eq!(date.start_of_month(), NaiveDate::from_ymd_opt(2000, 1, 1).unwrap());
+	}
+	
+	//		start_of_month_opt													
+	#[test]
+	fn start_of_month_opt__valid() {
+		assert_eq!(NaiveDate::start_of_month_opt(2000, 1).unwrap(), NaiveDate::from_ymd_opt(2000, 1, 1).unwrap());
+	}
+	#[test]
+	fn start_of_month_opt__invalid() {
+		assert!(NaiveDate::start_of_month_opt(2000, 13).is_none());
+	}
+	
+	//		end_of_month														
+	#[test]
+	fn end_of_month() {
+		let date = NaiveDate::from_ymd_opt(2000, 1, 20).unwrap();
+		assert_eq!(date.end_of_month(), NaiveDate::from_ymd_opt(2000, 1, 31).unwrap());
+	}
+	
+	//		end_of_month_opt													
+	#[test]
+	fn end_of_month_opt__valid() {
+		assert_eq!(NaiveDate::end_of_month_opt(2000, 1).unwrap(), NaiveDate::from_ymd_opt(2000, 1, 31).unwrap());
+	}
+	#[test]
+	fn end_of_month_opt__invalid() {
+		assert!(NaiveDate::end_of_month_opt(2000, 13).is_none());
+	}
+	
+	//		start_of_year														
+	#[test]
+	fn start_of_year() {
+		let date = NaiveDate::from_ymd_opt(2000, 2, 20).unwrap();
+		assert_eq!(date.start_of_year(), NaiveDate::from_ymd_opt(2000, 1, 1).unwrap());
+	}
+	
+	//		start_of_year_opt													
+	#[test]
+	fn start_of_year_opt() {
+		assert_eq!(NaiveDate::start_of_year_opt(2000).unwrap(), NaiveDate::from_ymd_opt(2000, 1, 1).unwrap());
+	}
+	
+	//		end_of_year														
+	#[test]
+	fn end_of_year() {
+		let date = NaiveDate::from_ymd_opt(2000, 2, 20).unwrap();
+		assert_eq!(date.end_of_year(), NaiveDate::from_ymd_opt(2000, 12, 31).unwrap());
+	}
+	
+	//		end_of_year_opt													
+	#[test]
+	fn end_of_year_opt() {
+		assert_eq!(NaiveDate::end_of_year_opt(2000).unwrap(), NaiveDate::from_ymd_opt(2000, 12, 31).unwrap());
+	}
 }
 
 
